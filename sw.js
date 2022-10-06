3893f2b1753731ee3a279127a50691ad1a5db1f6
asarfson <asarfson@ucm.es>
2022-10-06 13:38:41 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,13 +27,13 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9997ab860a813b937441.js"
+    "url": "webpack-runtime-1becfeb1a3a2f301fcd6.js"
   },
   {
     "url": "framework-bc5b66489610c4031e06.js"
   },
   {
-    "url": "styles.301369c187428e001b73.css"
+    "url": "styles.298fb0af308d56d70728.css"
   },
   {
     "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-71f0cc408a6fc074c3b5.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "318d613b8f3826580ceee2ed27fe657f"
+    "revision": "2a24a903e231377df068c6b80403dd56"
   },
   {
     "url": "polyfill-3d2cfbd3729d7397298f.js"
