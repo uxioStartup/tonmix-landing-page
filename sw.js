5ca8b20329a155ea2589dcae14301f7b97a39ba2
asarfson <asarfson@ucm.es>
2022-10-08 08:14:37 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,23 +27,23 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0a882aa2c10a0df3a429.js"
+    "url": "webpack-runtime-7cc6456c418ce63cbae1.js"
   },
   {
     "url": "framework-bc5b66489610c4031e06.js"
   },
   {
-    "url": "styles.298fb0af308d56d70728.css"
+    "url": "styles.60d16c26088eb290a43e.css"
   },
   {
     "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-71f0cc408a6fc074c3b5.js"
   },
   {
-    "url": "app-5ea9bd5baa00a1378cf8.js"
+    "url": "app-3387164945570f63b490.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "88b4830c55d324b244c4f4403f89a930"
+    "revision": "f80a5bee99ec04a60fb74ce4dfecddc3"
   },
   {
     "url": "polyfill-3d2cfbd3729d7397298f.js"
@@ -160,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/tonmix-landing-page/app-5ea9bd5baa00a1378cf8.js`))) {
+  if (!resources || !(await caches.match(`/tonmix-landing-page/app-3387164945570f63b490.js`))) {
     return await fetch(event.request)
   }
 
